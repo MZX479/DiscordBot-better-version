2c2bb7e43dce641fd88fd9f6355cbd83d98c6efd
Pavel <misteryx72@gmail.com>
2022-06-21 11:45:02 +0300

    added index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,58 @@
+import { Intents, Client } from 'discord.js';
+import { MongoClient } from 'mongodb';
+import * as fs from 'fs';
+import { token } from './config/token.json';
+import f from './config/modules';
+import { REST } from '@discordjs/rest';
+import { Routes } from 'discord-api-types/v9';
+import { Command } from './types';
 
+const bot = new Client({
+  intents: Object.values(Intents.FLAGS),
+});
+
+MongoClient.connect('mongodb://localhost:27017', (err, mongo) => {
+  fs.readdir('./events', async (err, events_dir) => {
+    for (let event_file of events_dir) {
+      if (!event_file.endsWith('.js')) continue;
+
+      let event_func = require('./events/' + event_file).default;
+      let event_name = event_file.split(' ')[0].replace('.js', '');
+
+      bot.on(event_name, event_func.bind(null, bot, f, mongo));
+    }
+  });
+});
+
+fs.readdir('./commands', async (err, commands_dir) => {
+  for (let command_file of commands_dir) {
+    if (!command_file.endsWith('.js')) continue;
+    let { command } = await import('./commands/' + command_file);
+    const Command = <Command>command;
+
+    f.commands.push(Command);
+  }
+
+  let rest: REST = new REST({
+    version: '9',
+  }).setToken(token);
+
+  try {
+    console.log('Started loading /-commands');
+    await rest.put(
+      Routes.applicationGuildCommands(
+        '827619223714398278',
+        '827620881529307217'
+      ),
+      {
+        body: f.commands.map((command) => command.slash),
+      }
+    );
+    console.log('Succesfully loaded /-commands');
+  } catch (error) {
+    console.log('Error while loading commands');
+    console.log(error);
+  }
+});
+
+bot.login(token);
---
+++ src/types.ts
@@ -0,0 +1,59 @@
+import {
+  Client,
+  CommandInteraction,
+  CommandInteractionOption,
+  Message,
+} from 'discord.js';
+import { MongoClient, ObjectId } from 'mongodb';
+
+export type configType = {
+  prefix: string;
+  owner: string;
+};
+
+export type Command_type = (
+  bot: Client,
+  f: modulesType,
+  mongo: MongoClient
+) => Promise<any>;
+
+export type modulesType = {
+  config: configType;
+  commands: Command[];
+};
+
+export type argsType = Readonly<CommandInteractionOption[]>;
+
+export type UserType = {
+  login?: string;
+  coins?: number;
+  daily_cooldown?: number;
+  reputation?: number;
+  isMuted?: {};
+  mutes?: Array<{}>;
+  rep_cooldown?: number;
+};
+
+export type slashType = {
+  name: string;
+  description: string;
+  options?: Array<{
+    name: string;
+    description: string;
+    required?: boolean;
+    type: number;
+  }>;
+};
+
+export type Command = {
+  slash: slashType;
+  ephemeral?: boolean;
+  execute: (
+    bot: Client,
+    f: modulesType,
+    mongo: MongoClient,
+    args: argsType,
+    interaction: CommandInteraction,
+    message?: Message
+  ) => Promise<any | void>;
+};
